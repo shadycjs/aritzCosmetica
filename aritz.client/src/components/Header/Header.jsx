e88fcc011155bb38a6046b9f7d4a9a1a3201d2eb
Ramiro Unrein <unreinramiro2000@gmail.com>
2025-08-01 16:17:14 -0300

    Header color verde y se implemento el isActive para manejar el color de los links segun la ruta

--- aritz.client/src/components/Header/Header.jsx
+++ aritz.client/src/components/Header/Header.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // React Router para navegación
+import { NavLink } from "react-router-dom";
+import styles from './Header.module.css';
 
 const Header = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div className="container">
+        <nav className={`navbar navbar-expand-lg ${styles.navbar}`}>
+            <div className="container d-flex justify-content-evenly">
                 {/* Logo */}
-                <Link className="navbar-brand" to="/">
-                    Mi Tienda
-                </Link>
+                <NavLink className="navbar-brand" to="/">
+                    Aritz
+                </NavLink>
 
                 {/* Botón para hamburguesa en dispositivos móviles */}
                 <button
@@ -24,22 +25,32 @@ const Header = () => {
                 </button>
 
                 {/* Enlaces de navegación */}
-                <div className="collapse navbar-collapse" id="navbarNav">
+                <div className="collapse navbar-collapse d-flex justify-content-center" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <Link className="nav-link" to="/">
+                            <NavLink
+                                className={({ isActive }) =>
+                                    isActive ? "nav-link active" : "nav-link"
+                                }
+                                to="/">
                                 Home
-                            </Link>
+                            </NavLink>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/product">
+                            <NavLink
+                                className={({ isActive }) =>
+                                    isActive ? "nav-link active" : "nav-link"
+                                }                                to="/product">
                                 Product
-                            </Link>
+                            </NavLink>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/contact">
+                            <NavLink className={({ isActive }) =>
+                                isActive ? "nav-link active" : "nav-link"
+                                }
+                                to="/contact">
                                 Contact
-                            </Link>
+                            </NavLink>
                         </li>
                     </ul>
                 </div>
