592a327a5e5a1ffc4f9b9d120f130a5964d4a194
Ramiro Unrein <unreinramiro2000@gmail.com>
2025-08-10 01:59:51 -0300

    Algunos retoques a la pagina Products.jsx

--- aritz.client/src/pages/Products/Products.jsx
+++ aritz.client/src/pages/Products/Products.jsx
@@ -3,6 +3,7 @@ import styles from './Products.module.css'
 import product1 from '../../assets/images/product1.png'
 import product2 from '../../assets/images/product2.png'
 import product3 from '../../assets/images/product3.jpg'
+import { FaFilter } from "react-icons/fa";
 function Products() {
     return (
         <>
@@ -14,7 +15,7 @@ function Products() {
             <div className={styles.productsPage}>
                 {/* Aside para los filtros */}
                 <aside className={styles.filters}>
-                    <h3 className={styles.filtersTitle}>Filtros</h3>
+                    <h3 className={styles.filtersTitle}>FILTROS <FaFilter /></h3>
                     <ul className={styles.filterList}>
                         <li className={styles.filterItem}>
                             <label>
@@ -42,32 +43,35 @@ function Products() {
                         <div className="col-sm-9">
                             <div className="row">
                                 {/* Producto 1 */}
-                                <div className="col-md-4">
-                                    <div className="card">
+                                <div className={`col-md-4 ${styles.columna}`}>
+                                    <div className={`card ${styles.carta}`}>
                                         <img src={product1} className="card-img-top" alt="Producto 1" />
-                                        <div className="card-body">
+                                        <div className={`card-body ${styles.cuerpoCarta}`}>
                                             <h5 className="card-title">Pasta dental</h5>
                                             <p className="card-text">$19.99</p>
                                         </div>
+                                        <button className="btn btn-primary">Agregar al carrito</button>
                                     </div>
                                 </div>
                                 {/* Repetir productos */}
-                                <div className="col-md-4">
-                                    <div className="card">
+                                <div className={`col-md-4 ${styles.columna}`}>
+                                    <div className={`card ${styles.carta}`}>
                                         <img src={product2} className="card-img-top" alt="Producto 1" />
-                                        <div className="card-body">
+                                        <div className={`card-body ${styles.cuerpoCarta}`}>
                                             <h5 className="card-title">Crema facial</h5>
                                             <p className="card-text">$44.99</p>
                                         </div>
+                                        <button className="btn btn-primary">Agregar al carrito</button>
                                     </div>
                                 </div>
-                                <div className="col-md-4">
-                                    <div className="card">
+                                <div className={`col-md-4 ${styles.columna}`}>
+                                    <div className={`card ${styles.carta}`}>
                                         <img src={product3} className="card-img-top" alt="Producto 1" />
-                                        <div className="card-body">
+                                        <div className={`card-body ${styles.cuerpoCarta}`}>
                                             <h5 className="card-title">Unguento calendula</h5>
                                             <p className="card-text">$99.99</p>
                                         </div>
+                                        <button className="btn btn-primary">Agregar al carrito</button>
                                     </div>
                                 </div>
                             </div>
