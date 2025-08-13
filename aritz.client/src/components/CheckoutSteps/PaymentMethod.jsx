fa24beeaf2c4a12e1e72b3ce62abd133abecb8d8
Ramiro Unrein <unreinramiro2000@gmail.com>
2025-08-13 18:07:01 -0300

    Se avanzo con 'PaymentMethod.jsx'

--- aritz.client/src/components/CheckoutSteps/PaymentMethod.jsx
+++ aritz.client/src/components/CheckoutSteps/PaymentMethod.jsx
@@ -4,6 +4,8 @@ import CenteredContainer from "../CenteredContainer/CenteredContainer";
 import styles from "./CheckoutSteps.module.css";
 import TimeLapseCheckout from "../CheckoutSteps/Timelapse/TimelapseCheckout";
 import { useNavigate } from "react-router-dom";
+import MercadoPagoIcon from "../../assets/icons/mercadopago.svg"
+import CreditCardIcon from "../../assets/icons/creditcard.svg"
 
 function PaymentInfo() {
     const { paymentMethod, setPaymentMethod } = useCheckout();
@@ -12,6 +14,7 @@ function PaymentInfo() {
     const handleSelectMethod = (method) => {
         setPaymentMethod(method);
         //alert("Pedido completado con éxito!"); // Aquí irías a la lógica de confirmación
+
     };
 
     const back = () => {
@@ -22,9 +25,11 @@ function PaymentInfo() {
         <CenteredContainer>
             <TimeLapseCheckout />
             <div className={styles.container}>
-                <h2>Metodo de Pago</h2>
-                <button onClick={() => handleSelectMethod("Credit Card")}>Tarjeta de Crédito</button>
-                <button onClick={() => handleSelectMethod("PayPal")}>MercadoPago</button>
+                <h2>Elija la forma de pago</h2>
+                <div className={styles.containerPayments}>
+                    <button className={styles.buttonsPayments} style={{ backgroundColor: "#F5F5DC", color: "#000" }} onClick={() => handleSelectMethod("Tarjeta")} onClick={() => handleSelectMethod("Credit Card")}><img src={CreditCardIcon} className={styles.imgMp} />Tarjeta de Credito</button>
+                    <button className={styles.buttonsPayments} style={{ backgroundColor: "#00B1EA", color: "#fff" }} onClick={() => handleSelectMethod("MercadoPago")}><img src={MercadoPagoIcon} className={styles.imgMp} /></button>
+                </div>
                 <label className={`d-flex gap-3 ${styles.shippingLabels}`}>
                     <button onClick={back} className={styles.btnShippingBack}>Volver</button>
                     <button className={styles.btnShippingNext} type="submit">Siguiente</button>
