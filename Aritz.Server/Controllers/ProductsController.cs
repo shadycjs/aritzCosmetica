75c363904777a4ae136ea93a178ff51a94ff0003
Ramiro Unrein <unreinramiro2000@gmail.com>
2025-08-25 10:21:35 -0300

    commit antes de instalar axios para probar las peticiones a la base

---
+++ Aritz.Server/Controllers/ProductsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Aritz.Server.Data;
+using Aritz.Server.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aritz.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly AritzDbContext _context;
+
+        // Inyección de dependencia del DbContext
+        public ProductsController(AritzDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/products
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        // GET: api/products/{id}
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return product;
+        }
+    }
+}
--- Aritz.Server/obj/Debug/net8.0/Aritz.Server.AssemblyInfo.cs
+++ Aritz.Server/obj/Debug/net8.0/Aritz.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aritz.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54d86e983603613a027a3fa5bc0d3ab556b18088")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d8ccc26011d533a1faf886feabb6aefd1110aec")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aritz.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aritz.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
