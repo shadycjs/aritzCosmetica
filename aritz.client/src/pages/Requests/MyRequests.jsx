ff289ed7e972d1ee7b0eb1f44f969a1f976d8c76
Ramiro Unrein <unreinramiro2000@gmail.com>
2025-09-16 17:12:18 -0300

    Se le agrego un disenio lindo a la vista MyRequests.jsx

--- aritz.client/src/pages/Requests/MyRequests.jsx
+++ aritz.client/src/pages/Requests/MyRequests.jsx
@@ -8,7 +8,25 @@ function MyRequests() {
         <CenteredContainer>
             <h1 className={styles.requestsTitle}>Mis pedidos</h1>
             <div className={styles.requestsContainer}>
-                
+                <table className={styles.requestsTable}>
+                    <thead>
+                        <tr>
+                            <th>Nro Orden</th>
+                            <th>Fecha</th>
+                            <th>Monto total</th>
+                            <th>Estado</th>
+                            <th>Forma de pago</th>
+                            <th>Comprobante</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+                                
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </CenteredContainer>
   );
