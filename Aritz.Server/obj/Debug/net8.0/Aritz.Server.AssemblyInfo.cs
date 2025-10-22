4a4da0be5599bfb2124eac1cec0e2f2e5a96a7a9
Ramiro Unrein <unreinramiro2000@gmail.com>
2025-10-22 15:42:04 -0300

    Se corrigio el CSS del dropdown del header

--- Aritz.Server/obj/Debug/net8.0/Aritz.Server.AssemblyInfo.cs
+++ Aritz.Server/obj/Debug/net8.0/Aritz.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aritz.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f07eeae49060a7ad816a15003669cd454c87c37d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecb4c6340d7077d564f24e6a63c18c7cb27949a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aritz.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aritz.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
