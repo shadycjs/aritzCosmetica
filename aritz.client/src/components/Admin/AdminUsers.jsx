7b19e493f0a7c49850827e9a2813e1ccb99adb1d
Ramiro Unrein <unreinramiro2000@gmail.com>
2025-12-30 14:21:31 -0300

    Se agrego el modal del usuario y se muestran los datos al hacer click en alguno

--- aritz.client/src/components/Admin/AdminUsers.jsx
+++ aritz.client/src/components/Admin/AdminUsers.jsx
@@ -2,9 +2,11 @@ import { useEffect, useState } from 'react';
 import axiosInstance from '../../api/axiosConfig';
 import styles from '../Admin/AdminManage.module.css'
 import { FaEdit } from "react-icons/fa";
+import ModalUsr from './ModalUsr';
 function AdminUsers() {
 
     const [users, setUsers] = useState([]);
+    const [selectedUser, setSelectedUser] = useState(null);
 
     const fetchUsers = async () => {
         try {
@@ -20,46 +22,55 @@ function AdminUsers() {
     }, []);
 
     return (
-        <table className={styles.productsUserTable}>
-            <thead>
-                <tr>
-                    <th>Id</th>
-                    <th>Nombre</th>
-                    <th>Apellido</th>
-                    <th>Email</th>
-                    <th>Telefono</th>
-                    <th>Administrador</th>
-                    <th>Documento</th>
-                    <th>Provincia</th>
-                    <th>Ciudad</th>
-                </tr>
-            </thead>
-            <tbody>
-                {users.map((usr) => (
-                    <tr key={usr.USR_ID}>
-                        <td>
-                            {usr.USR_ID}
-                        </td>
-                        <td>{usr.USR_NAME}</td>
-                        <td>{usr.USR_SURNAME}</td>
-                        <td>{usr.USR_EMAIL}</td>
-                        <td>{usr.USR_PHONE_NUMBER}</td>
-                        <td>{usr.USR_IS_ADMIN}</td>
-                        <td>{usr.USR_DOCUMENT_NUMBER}</td>
-                        <td>{usr.USR_PROVINCE}</td>
-                        <td>{usr.USR_CITY}</td>
-
-                        <td>
-                            <FaEdit
-                                size={20}
-                                style={{ cursor: "pointer" }}
-                            />
-                        </td>
+        <>
+            <table className={styles.productsUserTable}>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Nombre</th>
+                        <th>Apellido</th>
+                        <th>Email</th>
+                        <th>Telefono</th>
+                        <th>Administrador</th>
+                        <th>Documento</th>
+                        <th>Provincia</th>
+                        <th>Ciudad</th>
                     </tr>
-                ))}
+                </thead>
+                <tbody>
+                    {users.map((usr) => (
+                        <tr key={usr.USR_ID}>
+                            <td>
+                                {usr.USR_ID}
+                            </td>
+                            <td>{usr.USR_NAME}</td>
+                            <td>{usr.USR_SURNAME}</td>
+                            <td>{usr.USR_EMAIL}</td>
+                            <td>{usr.USR_PHONE_NUMBER}</td>
+                            <td>{usr.USR_IS_ADMIN}</td>
+                            <td>{usr.USR_DOCUMENT_NUMBER}</td>
+                            <td>{usr.USR_PROVINCE}</td>
+                            <td>{usr.USR_CITY}</td>
 
-            </tbody>
-        </table>
+                            <td>
+                                <FaEdit
+                                    size={20}
+                                    style={{ cursor: "pointer" }}
+                                    data-bs-toggle="modal"
+                                    data-bs-target="#staticBackdropUsr"
+                                    onClick={() => setSelectedUser(usr)}
+                                />
+                            </td>
+                        </tr>
+                    ))}
+
+                </tbody>
+            </table>
+
+            <ModalUsr
+                user={selectedUser}
+            />
+        </>
     )
 }
 
---
+++ aritz.client/src/components/Admin/ModalUsr.jsx
@@ -0,0 +1,30 @@
+function ModalUsr({ user }) {
+    return (
+        <div
+            className="modal fade"
+            id="staticBackdropUsr"
+            data-bs-backdrop="static"
+            data-bs-keyboard="false"
+            tabIndex="-1"
+            aria-labelledby="staticBackdropLabel"
+            aria-hidden="true"
+        >
+            <div className="modal-dialog">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h1 className="modal-title fs-5" id="staticBackdropLabel">{user.USR_NAME} {user.USR_SURNAME}</h1>
+                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div className="modal-body">
+                        ...
+                    </div>
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" className="btn btn-primary">Understood</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default ModalUsr;
