38d57a96b1c29da067ffe78a158a2e8691649b6d
Ramiro Unrein <unreinramiro2000@gmail.com>
2026-01-27 12:44:13 -0300

    Cambios en el carroussel antes de probar con un video

--- aritz.client/src/pages/Carroussel/Carroussel.jsx
+++ aritz.client/src/pages/Carroussel/Carroussel.jsx
@@ -1,34 +1,44 @@
+import { useEffect, useState } from 'react';
+import axiosInstance from '../../api/axiosConfig';
 import imagen1 from '../../assets/images/imagenCarrousel1.jpg';
 import imagen2 from '../../assets/images/imagenCarrousel2.jpg';
 import imagen3 from '../../assets/images/imagenCarrousel3.jpg';
 import styles from './Carroussel.module.css'; // Importa el CSS personalizado
 
 function Carroussel() {
+
+    const [img, setPrdImg] = useState([]);
+
+    useEffect(() => {
+        fetchImgPrd();
+    }, []);
+
+    const fetchImgPrd = async () => {
+        try {
+            const response = await axiosInstance.get('products');
+            setPrdImg(response.data);
+
+        } catch (e) {
+            console.log("Error al traer la imagen: ", e)
+        }
+    }
+
     return (
         <div className={styles.carrouselContainer}>
             <div id="carouselExampleFade" className={`carousel slide carousel-fade ${styles.carrouselContainerSub}`}>
-                <div className="carousel-inner" style={{ width: "100%", height: "100%" }}>
-                    <div className="carousel-item active" style={{ width: "100%", height: "100%" }}>
-                        <img
-                            src={imagen1}
-                            className={`d-block w-100 ${styles.carrouselImage}`}
-                            alt="..."
-                        />
-                    </div>
-                    <div className="carousel-item" style={{ width: "100%", height: "100%" }}>
-                        <img
-                            src={imagen2}
-                            className={`d-block w-100 ${styles.carrouselImage}`}
-                            alt="..."
-                        />
-                    </div>
-                    <div className="carousel-item" style={{ width: "100%", height: "100%" }}>
-                        <img
-                            src={imagen3}
-                            className={`d-block w-100 ${styles.carrouselImage}`}
-                            alt="..."
-                        />
-                    </div>
+                <div className="carousel-inner">
+                    {img.map((im, index) => (
+                        <div
+                            className={`imgCarContainer carousel-item ${index === 0 ? "active" : ""}`}
+                            key={im.PRD_ID}
+                        >
+                            <img
+                                src={`https://localhost:7273/images/${im.PRD_IMAGE}`}
+                                className={`${styles.carrouselImage}`}
+                                alt="..."
+                            />
+                        </div>
+                    ))}
                 </div>
                 <button
                     className="carousel-control-prev"
